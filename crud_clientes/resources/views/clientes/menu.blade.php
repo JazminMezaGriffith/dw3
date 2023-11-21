6c0ebf1dd95304b9c5b7a2c4b7a80b62a05ccfee
Jazmín M. Griffith <jazminm.griffith2003@gmail.com>
2023-11-21 17:49:09 -0300

    ABM clientes y cargos. Reportes dompdf

--- crud_clientes/resources/views/clientes/menu.blade.php
+++ crud_clientes/resources/views/clientes/menu.blade.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="mb-3">
     <ul class="nav nav-tabs">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="{{url('clientes/index') }}">Listado de clientes</a>
